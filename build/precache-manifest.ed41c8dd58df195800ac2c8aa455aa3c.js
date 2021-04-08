23b716b435cbea1b7b8dd8ab98ddc7501ce50b7a
arvind.nikam <arvind.nikam@halodoc.com>
2021-04-08 23:10:46 +0530

    first buil

---
+++ build/precache-manifest.ed41c8dd58df195800ac2c8aa455aa3c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b0bb03ae73a9f807a49",
+    "url": "/static/css/main.bfb3c4c2.chunk.css"
+  },
+  {
+    "revision": "0b0bb03ae73a9f807a49",
+    "url": "/static/js/main.5982124e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "537472d60ba8be4b5871",
+    "url": "/static/css/2.ed695509.chunk.css"
+  },
+  {
+    "revision": "537472d60ba8be4b5871",
+    "url": "/static/js/2.ddc743cf.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f5f5048662873b21627893054d223199",
+    "url": "/index.html"
+  }
+];
